4845c0e52372b69dcfb801bee551b9695a0e9e45
u24ai049 <u24ai049@aid.svnit.ac.in>
2024-09-05 09:10:56 +0530

    Add files via upload

---
+++ program 2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ float a, b, c, d, e, per;
+    printf("Enter Marks Of Sub1=");
+    scanf("%f",&a);
+   printf("Enter Marks Of Sub2=");
+    scanf("%f",&b);
+   printf("Enter Marks Of Sub3=");
+    scanf("%f",&c);
+   printf("Enter Marks Of Sub4=");
+    scanf("%f",&d);
+   printf("Enter Marks Of Sub5=");
+    scanf("%f",&e);
+    per=(a+b+c+d+e)/5;
+    printf("Percentage=%f",per);
+
+    getch() ;
+
+
+
+}
