10386fc9499882728af70d1fed77e9148c369101
u24ai049 <u24ai049@aid.svnit.ac.in>
2024-09-05 14:53:46 +0530

    Program 16.c

---
+++ Program 16.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main ()
+{
+    int a, b, c;
+    printf("Enter three numbers");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b)
+    { 
+        if(a>c)
+        {
+            printf("%d is biggest",a);
+            
+        }
+        else
+        {
+            printf("%d is biggest",c);
+        }
+        
+    }
+     else
+     {
+         if(b>c)
+         {
+             printf("%d is biggest",b) ;
+             
+         }
+         else
+         {
+             printf ("%d is biggest",c) ;
+             
+         }
+     }
+   
+}
