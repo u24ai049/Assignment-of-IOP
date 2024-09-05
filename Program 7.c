8650b276ea6a954ff6ee88161485a1d9f0bef622
u24ai049 <u24ai049@aid.svnit.ac.in>
2024-09-05 09:17:30 +0530

    Add files via upload

---
+++ Program 7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+float main()
+{
+float base, height, area;
+printf("Enter the value of Base=");
+scanf("%f",&base);
+printf("Enter the value of height=");
+scanf("%f",&height);
+area=base*height/2;
+printf("The area of the triangle is=%f",area);
+
+}
