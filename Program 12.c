e79166b57ec079e8a10541fa8cc5368cd1367051
u24ai049 <u24ai049@aid.svnit.ac.in>
2024-09-05 13:40:55 +0530

    Create Program 12.c

---
+++ Program 12.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+char character;
+    printf("Enter the number you want ascii value of");
+    scanf("%c",&character);
+    printf("The ASCII value of %c is %d",character,character);
+
+    return 0;
+}
