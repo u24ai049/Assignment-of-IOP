e7e2ae6dbe94eb52ede1746801aeb1318645d776
u24ai049 <u24ai049@aid.svnit.ac.in>
2024-09-05 13:53:40 +0530

    Program 13.c

---
+++ Program 13.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("\n Enter the number");
+    scanf("%d",&a);
+    if(a%2==1)
+    {printf("\n The number is odd");}
+    else
+    {printf("\n The number is even");}
+
+    return 0;
+}
