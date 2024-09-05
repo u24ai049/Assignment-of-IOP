b2791cceb261e329972a1a22836e603577f78280
u24ai049 <u24ai049@aid.svnit.ac.in>
2024-09-05 16:49:49 +0530

    Update Program 23.c

--- Program 23.c
+++ Program 23.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 int main()
 {
-    int a,b,result=1;
+    int a,b,c=1;
     printf("enter the values of a and b:\n");
     scanf("%d%d",&a,&b);
     for(int i=1;i<=b;i++)
     {
-        result=result*a;
+        c=c*a;
     }
-    printf("the value of a to the power b is:%d",result);
+    printf("the value of a to the power b is:%d",c);
     return 0;
 }
