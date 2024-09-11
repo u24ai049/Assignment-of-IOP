7b4569ff7dac709ee082b63dcdb312ac7611ab70
u24ai049 <u24ai049@aid.svnit.ac.in>
2024-09-11 21:46:09 +0530

    Create Program 27.c

---
+++ Program 27.c
@@ -0,0 +1,28 @@
+include<stdio.h>
+
+int main()
+{
+    int num, b;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (b = 2; b < num; b++)
+      {
+        if (num % b == 0) 
+        {
+            break;
+        }
+    }
+
+    if (b == num) 
+    {
+        printf("Prime\n");
+    } else 
+    {
+        printf("Not Prime\n");
+    }
+
+    return 0;
+}
+
+
