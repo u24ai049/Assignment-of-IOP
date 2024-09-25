36fe9ddcc32d27682713f8182065d7a1f5c1288d
u24ai049 <u24ai049@aid.svnit.ac.in>
2024-09-25 23:56:55 +0530

    Update program54.c

--- program54.c
+++ program54.c
@@ -3,12 +3,12 @@ int main()
 {
     int arr[10],sum=0;
     printf("Enter the value of integers\n");
-    for(int i=1;i<=10;i++)
+    for(int i=0;i<10;i++)
     scanf("%d",&arr[i]);
     printf("The value of array is: ");
-    for(int i=1;i<=10;i++)
+    for(int i=0;i<10;i++)
     printf("\t%d",arr[i]);
-    for(int i=1;i<=10;i++)
+    for(int i=0;i<10;i++)
     sum=sum+arr[i];
     printf("\nThe value of sum of all the integers is: %d ",sum);
     
