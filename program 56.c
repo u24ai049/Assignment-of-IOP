bb2938d986f37324748a4d5ae938422c491d7840
u24ai049 <u24ai049@aid.svnit.ac.in>
2024-09-25 23:59:18 +0530

    Update program 56.c

--- program 56.c
+++ program 56.c
@@ -4,12 +4,12 @@ int main()
     int arr[10];
     int positive=0,negative=0,zero=0;
     printf("Enter the value of integers\n");
-    for(int i=1;i<=10;i++)
+    for(int i=0;i<10;i++)
     {scanf("%d",&arr[i]);}
     printf("The value of array is: ");
-    for(int i=1;i<=10;i++)
+    for(int i=0;i<10;i++)
     {printf("\t%d",arr[i]);}
-    for(int i=1;i<=10;i++)
+    for(int i=0;i<10;i++)
    {
        if(arr[i]>0)
        {positive=positive+1;}
