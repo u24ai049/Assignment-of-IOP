de588ce4888b25f3c0d17c3cb8de62e798e57992
u24ai049 <u24ai049@aid.svnit.ac.in>
2024-09-25 23:58:02 +0530

    Update program55.c

--- program55.c
+++ program55.c
@@ -5,12 +5,12 @@ int main()
     char sad='0';
     char odd='0';
     printf("Enter the value of 10 integers\n");
-    for(int i=1;i<=10;i++)
+    for(int i=0;i<10;i++)
     scanf("%d",&arr[i]);
     printf("The value of 10 integers: ");
-    for(int i=1;i<=10;i++)
+    for(int i=0;i<10;i++)
     printf("%d\t",arr[i]);
-    for(int i=1;i<=10;i++)
+    for(int i=0;i<10;i++)
    {
        if(arr[i]%2==0)
        {sad=sad+1;}
