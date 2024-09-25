96991088d1250556849a45c31c97deac88f43c70
u24ai049 <u24ai049@aid.svnit.ac.in>
2024-09-25 20:36:21 +0530

    Create program47.c

---
+++ program47.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{   int i,j;
+    for(j=1;j<=5;j++)
+{
+    for(i=1;i<=j;i++)
+    switch(i)
+    {
+        case 1:printf("A");
+            break;
+        case 2:printf("B");
+            break;
+        case 3:printf("C");
+            break;
+        case 4:printf("D");
+            break;
+        case 5:printf("E");
+            break;
+    }printf("\n");
+}
+}
