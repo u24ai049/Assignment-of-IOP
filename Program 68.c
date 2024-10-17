8425b39fe6ffda9c19b1a69923c16e083e96f6a4
u24ai049 <u24ai049@aid.svnit.ac.in>
2024-10-17 15:54:21 +0530

    Update Program 68.c

--- Program 68.c
+++ Program 68.c
@@ -29,7 +29,7 @@ int main()
     for (int i = 0; i < 3; i++) 
     {
         for (int j = 0; j < 3; j++) {
-            printf("%d ", subMatrix[i][j]);
+            printf("%d ", mulMatrix[i][j]);
         }
         printf("\n");
     }
